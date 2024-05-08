974aac128de8c44bda3b3658fee2f23eed0879a1
saja123 <saja_fawaz@yahoo.com>
2024-05-08 18:36:17 +0300

    solve lab16

---
+++ src/footer/Footer.js
@@ -0,0 +1,15 @@
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <footer>
+            <div className="icon">
+            <a href="https://www.facebook.com/"><img src="https://www.freepngimg.com/download/facebook/65283-icons-wallpaper-desktop-fb-computer-facebook-logo.png" alt="facbook" /></a>
+            <a href="https://www.instagram.com/?hl=en"><img src="https://th.bing.com/th/id/R.3e60846dd5a4adf6b0eb0f751c7a8141?rik=yrwL74rJBaxsag&pid=ImgRaw&r=0" alt="Instgram" /></a>
+            </div>
+            <div>© Devlopment By Saja.</div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/home/Home.js
@@ -0,0 +1,16 @@
+import Header from "../header/Header.js"
+import Tours from "../tours/Tours.js";
+import Footer from "../footer/Footer.js";
+
+function Home() {
+
+    return (
+        <>
+        <Header />
+        <Tours />
+        <Footer />
+        </>
+    );
+}
+
+export default Home;
